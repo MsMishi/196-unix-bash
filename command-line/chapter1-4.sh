c60e2b4182d41dfa362cc78477415aa94666f47e
MsMishi <mcwcharles@gmail.com>
2017-04-18 11:38:19 -0700

    'Add Chapter 1.4'

---
+++ command-line/chapter1-4.sh
@@ -0,0 +1,11 @@
+#Chapter 1.4
+#Exercise 1: Using the up awrrow, print to the screen the strings,"fee", "fie", "foe", "fum" without retyping echo each time.
+echo "fee"
+ ↑"fie"
+ ↑"foe"
+ ↑"fum"
+
+#Exercise 2: Starting with the line in Listing 6, use any combination of ⌃A, ⌃E, arrow keys, or Option-click to change the occurrences of the short "s" to the archaic long "ſ" in order to match the appearance of the original (Figure 12).
+$ echo "From fairest creatures we desire increase,"
+#becomes
+$ echo "FRom faireſt creatureſ we deſire increaſe,"
