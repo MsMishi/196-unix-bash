5e52570a1774b5f8d4c16480d756d98df151927f
MsMishi <mcwcharles@gmail.com>
2017-04-18 10:13:07 -0700

    Add Chapter 1.3 Exercise and .gitignore

---
+++ command-line/chapter1-3.sh
@@ -0,0 +1,4 @@
+#Chapter 1.3:
+#Exercise 1: According to the man page, what are the official short and long descriptions of echo #on your system?
+echo [-n] [string ...] #short
+echo -- write arguments to the standard output
