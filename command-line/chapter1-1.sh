b87302173d7094165923dfdef4f0617370af0f4d
MsMishi <mcwcharles@gmail.com>
2017-04-18 10:09:30 -0700

    Add Chapter 1.1 Exercises

---
+++ command-line/chapter1-1.sh
@@ -0,0 +1,6 @@
+#Chapter 1.1:
+#Exercise 2: Most modern terminal programs have the ability to create multiple tabs (Figure 7), which are useful for organizing a set of related terminal windows. By examining the menu items for your terminal program (Figure 8), figure out how to create a new tab.
+Select New Tab from the File Menu.
+
+#Extra credit: Learn the keyboard shortcut for creating a new tab.
+Shortcut: "⌘T" or "CMD + T"
