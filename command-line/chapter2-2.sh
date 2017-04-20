e8582521e52d7e791be29af9987041485625184e
MsMishi <mcwcharles@gmail.com>
2017-04-19 20:27:24 -0700

    Add Chapter 2.2 Exercises

---
+++ command-line/chapter2-2.sh
@@ -0,0 +1,9 @@
+#Chapter 2.2
+#Exercise 1: What's the command to list all the non-hidden files and directories that start with the letter "s"?
+$ ls s*
+
+#Exercise 2: What is the command to list all the non-hidden files that contain the string 'onnet', long-form by reverse modification time?
+$ ls *onnet* -rtl
+
+#Exercise 3: What is the command to list all files (including hidden ones) by reverse modification time, in long form?
+$ ls -a -rtl
