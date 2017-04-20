6f3dadb07ae78df2a70dd832f4e48f9deb6096dc
MsMishi <mcwcharles@gmail.com>
2017-04-20 13:23:54 -0700

    Add Chapter 4.1 exercises

---
+++ command-line/chapter4-1.sh
@@ -0,0 +1,9 @@
+#Chapter 4.1
+#Exercise 1: Write in words how you might speak the directory ~/foo/bar.
+home backslash foo backslash bar
+
+#Exercise 2: In /Users/bill/sonnets, what is the home directory? What is the username? Which directory is deepest in the hierarchy?
+Users is the home directory. The username is bill. The  sonnets directory is deepest in the hierarchy.
+
+#Exercise 3: For a user with username bill, how do /Users/bill/sonnets and ~/sonnets differ (if at all)?
+They are functionally the same. 
