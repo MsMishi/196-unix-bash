59de540a3313fd57aea144907490d365bf02b5fc
MsMishi <mcwcharles@gmail.com>
2017-04-21 15:21:18 -0700

    Add Bash scripting activity

---
+++ bash-scripting/act1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#A good place to start is to create a simple script which will accept some command line arguments and echo out some details about them (eg, how many are there, what is the second one etc).
+
+touch act4.sh
+chmod 777 act4.sh
+rm tempfile.txt
